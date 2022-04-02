833dba448e75f22a8cc21e7d99889cfa52e1097b
Shamyk2009 <shamykggf@gmail.com>
2022-04-02 16:41:04 +0300

    Logic writing completed. Added plus and minus.

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-import ProjectMath
+from ProjectMath import *
+import os
+
+def main():
+    os.system('cls')
+
+    action = input('Enter action(+, -) --> ')
+
+    a = float(input('Enter first number --> '))
+    b = float(input('Enter second number --> '))
+
+    if action == '+':
+        answer = plus(a, b)
+    
+    elif action == '-':
+        answer = minus(a, b)
+
+    else:
+        print('Incorrect action!')
+        answer = 'none'
+
+    print(f'The answer is {answer}.')
+
+
+
+if __name__ == '__main__':
+
+    main()
