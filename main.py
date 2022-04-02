397c779f60db4ba418529834d98b82758f5d7338
Shamyk2009 <shamykggf@gmail.com>
2022-04-02 16:54:05 +0300

    Added comments

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 from ProjectMath import *
 import os
 
+
+# Main function
 def main():
+
+    # Main cycle
     while True:
+
+        # Console clear
         os.system('cls')
 
+        # Enter action and numbers
         action = input('Enter action(+, -) --> ')
 
         a = float(input('Enter first number --> '))
         b = float(input('Enter second number --> '))
 
+        # Check action
         if action == '+':
             answer = plus(a, b)
         
         elif action == '-':
             answer = minus(a, b)
 
+        # If entered action isn't + or -, print Incorrect action.
         else:
             print('Incorrect action!')
             answer = 'none'
 
+        # Print answer
         print(f'The answer is {answer}.')
 
         input('Press Enter to continue ... ')
@@ -28,4 +38,5 @@ def main():
 
 if __name__ == '__main__':
 
+    # Call main function
     main()
